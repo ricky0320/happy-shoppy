6fe95ce2e97e6b90a29041853249a15800ce2213
Ricky Leung <ricxx0320@gmail.com>
2021-12-19 23:22:40 -0800

    added Product API routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,13 +6,52 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    // be sure to include its associated Category and Tag data
+    // include Category
+    include: [{
+      model: Category,
+      attributes: ['id', 'category_name']
+    }],
+    // include Tag
+    include: [{
+      model: Tag,
+      attributes: ['id', 'tag_name']
+    }]
+  })
+  .then(tagData => res.json(tagData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: { id: req.params.id },
+    // be sure to include its associated Category and Tag data
+    include: [{
+      model: Category,
+      attributes: ['id', 'Category_name']
+    }],
+    include: [{
+      model: Tag,
+      attributes: ['id', 'tag_name']
+    }]
+  })
+  .then(categoryData => {
+    if (!categoryData) {
+      res.status(404).json({ message: 'No such id'});
+      return;
+    }
+    res.json(categoryData);
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).res.json(err);
+  })
 });
 
 // create new product
@@ -91,6 +130,13 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({ 
+    where: { id: req.params.id },
+  })
+  .then((deletedProduct) => {
+    res.json(deletedProduct);
+  })
+  .catch((err) => res.json(err));
 });
 
 module.exports = router;
