38161b3ebffc52beee4cf8890709795148864db5
Ricky Leung <ricxx0320@gmail.com>
2021-12-20 00:48:26 -0800

    added Tag API routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,24 +5,76 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all tags
-  // be sure to include its associated Product data
+  Tag.findAll({
+    // be sure to include its associated Product data
+    include: [{
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock']
+    }]
+  })
+  .then(tagData =>res.json(tagData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
+  Tag.findOne({
+    where: { id: req.params.id },
+    // be sure to include its associated Product data
+    include: [{
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+    }]
+  })
+  .then(tagData => {
+    if(!tagData) {
+      res.status(404).json({ message: 'No such id'});
+      return;
+    }
+    res.json(tagData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
+  
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create(req.body)
+  .then((newTag) => { res.json(newTag) })
+  .catch((err) => { res.json(err) })
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update({
+    tag_name: req.body.tag_name
+      }, 
+      { where: { id: req.params.id },
+    })
+    .then((updateTag) => {
+        res.json(updateTag);
+    })
+    .catch((err) => {
+        console.log(err);
+        res.json(err);
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: { id: req.params.id },
+  })
+  .then((deletedTag) => {
+    res.json(deletedTag);
+  })
+  .catch((err) => res.json(err));
 });
 
 module.exports = router;
